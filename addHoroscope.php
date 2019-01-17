0ed3684356da189401b326a5e02e028f76327f89
alcr33k <alex.alle@hotmail.com>
2019-01-17 23:34:56 +0100

    Changed line of session_start

--- addHoroscope.php
+++ addHoroscope.php
@@ -1,7 +1,7 @@
 <?php
 require_once('generateHoroscope.php');
+session_start();
 if (isset($_POST['birthnumber']) && (!(isset($_SESSION['horoscope'])))) {
-    session_start();
     $birthnumber = $_POST['birthnumber'];
     $birthYear = substr($birthnumber, 0, 4);
     $birthDate= date('m-d', strtotime($birthnumber));
